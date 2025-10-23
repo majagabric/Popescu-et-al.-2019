f6cb01ac30e4b7cb3a40b736978bac1dd9140def
Maja Gabric <s2712272@ed.ac.uk>
2025-10-23 10:35:42 +0100

    Add metadata

--- P19_w4_create_Seurat_object.R
+++ P19_w4_create_Seurat_object.R
@@ -39,3 +39,26 @@ seurat_obj <- AddMetaData(seurat_obj, metadata = sample_id, col.name = "sample_i
 ##### Save Seurat object #####
 saveRDS(seurat_obj, file="P19_w4_suerat_obj.rds")
 
+##### Load Seurat object #####
+seurat_obj <- readRDS('/Users/majagabric/Documents/PhD/Data analysis/Popescu et al., 2019/Popescu et al., 2019/P19_w4_suerat_obj.rds')
+
+##### Add metadata #####
+# Get the cell tags
+barcodes <- colnames(seurat_obj)
+
+# Cell tags are made of 16 bases, a dash and the sample number, so you can simply split by "-" and take whatever's on the right
+# TGGCCAGGTTATCACG-1
+# We use sapply to go through each barcode and apply the strsplit function, which splits a string based on a delimiter. 
+# strsplit will return an array of length 2; the first element will be the actual cell tag, and the second (which we will return) is the sample number
+# We save the result of that in a metadata called Sample
+seurat_obj$Sample <- sapply(strsplit(barcodes, "-"), function(x){return(x[2])})
+
+# We can now create the other metadata depending on Sample
+# Define sex for each sample then create the Sex metadata accordingly
+seurat_obj$Sample <- as.numeric(seurat_obj$Sample)
+sex_metadata <- c("F", "F", "F", "F", "F")
+seurat_obj$Sex <- sex_metadata[seurat_obj$Sample]
+seurat_obj$Week_gestation <- 4
+seurat_obj$Tissue <- "YS"
+
+
